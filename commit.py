2dcef0d7635ca15d15282a2a2e3e93429b3ef801
Lenin Paulino <leninpaulinon@gmail.com>
2019-06-13 23:51:43 -0400

    Add commit.py

--- commit.py
+++ commit.py
@@ -1,8 +1,9 @@
 import os
 import sys
 
-file = sys.argv[0]
+file = sys.argv[1]
 cmd = 'git add %s; git commit -m \'Add %s\'' % (file, file)
+print(cmd)
 fp = os.popen(cmd)
 print(fp.read())
 status = fp.close()
